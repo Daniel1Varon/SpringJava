81124f661267bb772af12ef129a815b92116adbb
Daniel Varón <dani-0153@hotmail.com>
2022-09-09 00:21:42 -0500

    Implement and configure in SwaggerConfig.java swagger2 to manage the documentation of the api's endpoints.

--- src/main/java/com/platzi/market/web/controller/ProductController.java
+++ src/main/java/com/platzi/market/web/controller/ProductController.java
@@ -2,6 +2,10 @@ package com.platzi.market.web.controller;
 
 import com.platzi.market.domain.Product;
 import com.platzi.market.domain.service.ProductService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,12 +21,21 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
+    //Anotaciones para añadir información a la documentación
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code=200,message="Ok")
     public ResponseEntity<List<Product>> getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") long id){
+    //Anotaciones para añadir mas información a la documentacion (funcion y parametros)
+    @ApiOperation("Search a product with an ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Ok"),
+            @ApiResponse(code = 404, message = "Product not found")
+    })
+    public ResponseEntity<Product> getProduct(@ApiParam(value="The id of the product", required = true, example="7") @PathVariable("id") long id){
         return productService.getProduct(id)
                 .map(prod->new ResponseEntity<>(prod,HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
